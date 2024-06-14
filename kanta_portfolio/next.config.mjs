6cae9890f855f5f9d0a96e60a8a998d5dac49602
TaharaKanta <piraruku66@gmail.com>
2024-06-15 00:37:07 +0900

    fix: github actions10

--- kanta_portfolio/next.config.mjs
+++ kanta_portfolio/next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'kanta_portfolio';
 
 const nextConfig = {
+  output: "export",
   basePath: isProd ? `/${repoName}` : '',
   assetPrefix: isProd ? `/${repoName}/` : '',
 };
