2691b78fdfb4f272c109546421d9707c0d5c47e8
TaharaKanta <piraruku66@gmail.com>
2024-06-15 16:06:38 +0900

    fix:eslint hooks name

--- kanta_portfolio/src/utils/getLocale.ts
+++ kanta_portfolio/src/utils/getLocale.ts
@@ -2,7 +2,7 @@ import en from "@/constants/en";
 import ja from "@/constants/ja";
 import { useRouter } from "next/router";
 
-export default function getLocale() {
+export default function useLocale() {
   const { locale } = useRouter();
   const lan = locale === "en" ? en : ja;
   return { locale, lan}
