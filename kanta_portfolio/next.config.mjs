aaa7cfa6ffb968990a3b5453e49f4629f5421423
TaharaKanta <piraruku66@gmail.com>
2024-06-15 16:16:36 +0900

    change:for aws

--- kanta_portfolio/next.config.mjs
+++ kanta_portfolio/next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'kanta_portfolio';
-
 const nextConfig = {
-  output: "export",
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
-  images: {
-    unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : '',
-  },
-  // i18n: {
-  //   locales: ['en', 'ja'],
-  //   defaultLocale: 'ja',
-  // },
 };
 
 export default nextConfig;
--- kanta_portfolio/src/components/HeroSection.tsx
+++ kanta_portfolio/src/components/HeroSection.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import Introduction from "./Introduction";
-import { useRouter } from "next/router";
 export default function HeroSection() {
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
     return (
         <section className="w-full h-[600px]" aria-label="hero section">
             <div className="flex w-full h-full">
@@ -10,7 +8,7 @@ export default function HeroSection() {
                     <Introduction />
                 </div>
                 <div className="relative w-1/2 h-full" aria-label="hero image wrapper">
-                    <Image src={`${basePath}/hero-image.jpg`} fill={true} objectFit="contain" alt="hero image" />
+                    <Image src={`/hero-image.jpg`} fill={true} objectFit="contain" alt="hero image" />
                 </div>
             </div>
         </section>
