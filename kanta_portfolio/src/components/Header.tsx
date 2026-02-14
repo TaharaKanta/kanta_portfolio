1938b3d723c358205d054edef28096ec0eb5fb96
TaharaKanta <piraruku66@gmail.com>
2026-02-15 00:34:24 +0900

    改めて初期化

--- kanta_portfolio/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="h-10 w-full border-b-2 px-8">
-      <div className="flex w-full h-full items-center">
-        Kanta Tahara Portfolio
-      </div>
-    </header>
-  )
-}
